7b0be2066ff712337299091ca65b8808786aee92
marwanagha <47464986+marwanagha@users.noreply.github.com>
2020-03-23 13:55:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php//hellorequire_once "include/config.php";if($page=='login'){    require_once "include/layout/header.php";    require_once "include/pages/login.php";    require_once "include/layout/footer.php";    exit;}if($page=='404'){    require_once "include/layout/header.php";    require_once "include/pages/404.php";    require_once "include/layout/footer.php";    exit;}//var_dump($page);exit;require_once "include/layout/header.php";require_once "include/layout/side-menu.php";require_once "include/pages/{$page}.php";require_once "include/layout/footer.php";
+
+<?php
+//hello from git
+require_once "include/config.php";
+
+if($page=='login')
+{
+    require_once "include/layout/header.php";
+    require_once "include/pages/login.php";
+    require_once "include/layout/footer.php";
+    exit;
+}
+if($page=='404')
+{
+    require_once "include/layout/header.php";
+    require_once "include/pages/404.php";
+    require_once "include/layout/footer.php";
+    exit;
+}
+//var_dump($page);exit;
+
+require_once "include/layout/header.php";
+require_once "include/layout/side-menu.php";
+require_once "include/pages/{$page}.php";
+require_once "include/layout/footer.php";
+
+
+
+
